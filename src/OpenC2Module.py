eb7fce9efcabc8cc3a17c41bee721a86901a00b3
alexmateev <mitko.mateev@uconn.edu>
2020-02-14 12:00:00 -0500

    Update OpenC2Module.py

--- src/OpenC2Module.py
+++ src/OpenC2Module.py
@@ -1,4 +1,10 @@
 class OpenC2Module:
   #PHIS-21
+  def receiveProofpointSignal():
+    pass
   #PHIS-22
+  def receiveManualFormSignal():
+    pass
   #PHIS-24
+  def sendStatusNotification(email):
+    pass
